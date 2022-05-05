5085d3e31e75ed04784fa4d18abd6310a05e426d
Rajesh-107 <applerb326@gmail.com>
2022-05-06 01:24:44 +0600

    jwt token fix

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -9,7 +10,24 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(cors());
 app.use(express.json());
 
+// function verifyJWT(req, res, next) {
+//     const authHeader = req.headers.authorization;
+//     if (!authHeader) {
+//         return res.status(401).send({ message: 'unauthorized access' });
+//     }
+//     const token = authHeader.split(' ')[1];
+//     jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+//         if (err) {
+//             return res.status(403).send({ message: 'Forbidden access' });
+//         }
+//         console.log('decoded', decoded);
+//         req.decoded = decoded;
+//         next();
 
+//     });
+
+
+// }
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1u7kw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -19,6 +37,27 @@ async function run() {
         await client.connect();
         const partCollection = client.db('carparts-warehouse').collection('carparts');
         const orderCollection = client.db('carparts-warehouse').collection('order');
+
+        //AUTH
+        app.post('/login', async(req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        })
+
+
+
+        //jwt
+        // app.post('/login', async(req, res) => {
+        //     const user = req.body;
+        //     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        //         expiresIn: '1d'
+        //     });
+        //     res.send({ accessToken });
+        // })
+
 
         app.get('/inventory', async(req, res) => {
             const query = {};
@@ -48,6 +87,31 @@ async function run() {
             const result = await partCollection.deleteOne(query);
             res.send(result);
         });
+
+        // app.get('/order', verifyJWT, async(req, res) => {
+
+        //     const decodedEmail = req.decoded.email;
+        //     const email = req.query.email;
+        //     console.log(email);
+        //     if (email === decodedEmail) {
+        //         const query = { email: email };
+        //         const cursor = orderCollection.find(query);
+        //         const myItems = await cursor.toArray();
+        //         res.send(myItems);
+        //     } else {
+        //         res.status(403).send({ message: 'forbidden access' })
+        //     }
+        // })
+
+        app.get('/orders', async(req, res) => {
+            const email = req.query.email;
+            console.log(email);
+            const query = { email: email };
+            const cursor = orderCollection.find(query);
+            const myItems = await cursor.toArray();
+            res.send(myItems);
+        })
+
 
         //Order
         app.post('/order', async(req, res) => {
