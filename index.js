e01247e51d7e861ff10ee7463a0c5ee040286cd8
Rajesh-107 <applerb326@gmail.com>
2022-05-06 20:57:47 +0600

    jwt token implementation

--- index.js
+++ index.js
@@ -10,24 +10,23 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(cors());
 app.use(express.json());
 
+
 // function verifyJWT(req, res, next) {
 //     const authHeader = req.headers.authorization;
 //     if (!authHeader) {
-//         return res.status(401).send({ message: 'unauthorized access' });
+//         return res.status(401).send({ message: 'unauthorized access' })
 //     }
-//     const token = authHeader.split(' ')[1];
-//     jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
-//         if (err) {
-//             return res.status(403).send({ message: 'Forbidden access' });
-//         }
-//         console.log('decoded', decoded);
-//         req.decoded = decoded;
-//         next();
+//     // const token = authHeader.split(' ')[1];
+//     // jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//     //     if (err) {
+//     //         return res.status(403).send({ message: 'Forbidden access' });
+//     //     }
+//     //     console.log('decoded', decoded);
+//     // })
 
-//     });
-
-
+//     next();
 // }
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1u7kw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -47,18 +46,6 @@ async function run() {
             res.send({ accessToken });
         })
 
-
-
-        //jwt
-        // app.post('/login', async(req, res) => {
-        //     const user = req.body;
-        //     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN, {
-        //         expiresIn: '1d'
-        //     });
-        //     res.send({ accessToken });
-        // })
-
-
         app.get('/inventory', async(req, res) => {
             const query = {};
             const cursor = partCollection.find(query);
@@ -72,6 +59,8 @@ async function run() {
             const part = await partCollection.findOne(query);
             res.send(part);
         });
+
+
 
         //POST
         app.post('/inventory', async(req, res) => {
@@ -88,28 +77,18 @@ async function run() {
             res.send(result);
         });
 
-        // app.get('/order', verifyJWT, async(req, res) => {
-
-        //     const decodedEmail = req.decoded.email;
-        //     const email = req.query.email;
-        //     console.log(email);
-        //     if (email === decodedEmail) {
-        //         const query = { email: email };
-        //         const cursor = orderCollection.find(query);
-        //         const myItems = await cursor.toArray();
-        //         res.send(myItems);
-        //     } else {
-        //         res.status(403).send({ message: 'forbidden access' })
-        //     }
-        // })
-
         app.get('/orders', async(req, res) => {
-            const email = req.query.email;
-            console.log(email);
-            const query = { email: email };
+
+            const query = {};
             const cursor = orderCollection.find(query);
-            const myItems = await cursor.toArray();
-            res.send(myItems);
+            const items = await cursor.toArray();
+            res.send(items);
+            // const email = req.query.email;
+            // console.log(email);
+            // const query = { email: email };
+            // const cursor = orderCollection.find(query);
+            // const myItems = await cursor.toArray();
+            // res.send(myItems);
         })
 
 
